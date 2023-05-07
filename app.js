445797261902c14bea91fd2ed645375aaefedb45
Adolphus Okoro <adolphusokoro@Adolphuss-MacBook-Air.local>
2023-05-07 09:41:32 +0100

    remote or local

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
+import { SSMClient, GetParametersCommand } from "@aws-sdk/client-ssm";
+import * as dotenv from "dotenv";
+dotenv.config();
 
+let username
+let password
 
-async function accessParameter(){
+if (process.env.SERVER==='local'){
+    username = process.env.MONGO_USER
+    password = process.env.MONGO_PASSWORD
+    console.log(`username:  ${username}. password: ${password}`);
+} else {
+    accessParameters()
+}
+
+async function accessParameters(){
     try {
         const client = new SSMClient({ region: "us-east-1" });
         const input = {
-            Name: "/tmdb/api_key",
+            Names: ["/qubit-app/mongodb/username", "/qubit-app/mongodb/password"],
+            WithDecryption: true
         }
-        const command = new GetParameterCommand(input);
+        const command = new GetParametersCommand(input);
         const response = await client.send(command);
-        console.log(response);
+        console.log(response.Parameters);
     } catch (err) {
         console.log(err.message);
     }
 }
 
-accessParameter()
+// accessParameters()
