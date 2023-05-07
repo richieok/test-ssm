d5290393e80a5f40b03806e6485120e0a54789cf
Adolphus Okoro <adolphusokoro@Adolphuss-MacBook-Air.local>
2023-05-07 11:15:43 +0100

    proper

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-import { SSMClient, GetParametersCommand } from "@aws-sdk/client-ssm";
+import { SSMClient, GetParametersCommand, GetParameterCommand } from "@aws-sdk/client-ssm";
 import * as dotenv from "dotenv";
 dotenv.config();
 
 let username
 let password
 
-if (process.env.SERVER==='local'){
-    username = process.env.MONGO_USER
-    password = process.env.MONGO_PASSWORD
-    console.log(`username:  ${username}. password: ${password}`);
-} else {
-    accessParameters()
+try{
+    if (process.env.SERVER) {
+        username = process.env.MONGO_USER
+        password = process.env.MONGO_PASSWORD
+    } else {
+        username = accessParameter("/qubit-app/mongodb/username")
+        password = accessParameter("/qubit-app/mongodb/password")
+    }
+} catch (err) {
+    console.log(err.message);
+} finally {
+    if (username && password) {
+        console.log(`username:  ${username}. password: ${password}`);
+    }
+    else{
+        console.log('Got nothing');
+    }
 }
 
-async function accessParameters(){
+
+async function accessParameters() {
     try {
         const client = new SSMClient({ region: "us-east-1" });
         const input = {
@@ -26,6 +38,17 @@ async function accessParameters(){
     } catch (err) {
         console.log(err.message);
     }
+}
+
+async function accessParameter(paraName) {
+    const client = new SSMClient({ region: "us-east-1" });
+    const input = {
+        Name: paraName,
+        WithDecryption: true
+    }
+    const command = new GetParameterCommand(input);
+    const response = await client.send(command);
+    return response.Parameter.Value
 }
 
 // accessParameters()
